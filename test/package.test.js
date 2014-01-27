53ea71aac1cc747ac4598c31c72f55c534ab2f82
Jared Hanson <jaredhanson@gmail.com>
2014-01-27 11:57:38 -0800

    Initial commit (copy of crane-amqp).

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('antenna-amqp', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
